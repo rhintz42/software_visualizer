ef2a8ee490c7995d0b24720a8ba540e0bc758dfe
Robert Hintz <roberth@surveymonkey.com>
2014-05-04 12:19:35 -0700

    First Commit

---
+++ models/repositories.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var Repository = new Schema({
+    url:        { type: String },
+    name: {
+        first:  { type: String },
+        last:   { type: String }
+    },  
+    phone:      { type: String },
+    gravatar:   { type: String }
+});
+
+module.exports = { 
+    Repository: mongoose.model('Repository', Repository)
+};
---
+++ routes/repositories.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var repositories_model = require('../models/repositories');
+var router = express.Router();
+
+/* GET respositories listing. */
+router.get('/', function(req, res) {
+	repositories_model.Repository.find({}, function(err, data){
+		res.json(data);
+	})
+});
+
+/* GET respositories listing. */
+router.get('/:id', function(req, res) {
+	repositories_model.Repository.find({_id: req.params.id}, function(err, data){
+		res.json(data);
+	})
+});
+
+/* GET respositories listing. */
+router.get('/:id/edit', function(req, res) {
+	repositories_model.Repository.find({_id: req.params.id}, function(err, data) {
+		if (err) {
+			res.send("Shit2");
+		}
+
+		data[0].name = "Robdawg";
+		data[0].save(function (err) {
+			if (err)
+				res.send("Shit3");
+			res.redirect("/repositories/"+ data[0]._id);
+		})
+	});
+});
+
+/* GET respositories listing. */
+router.get('/add', function(req, res) {
+	var repository = new repositories_model.Repository({email: "roberth@surveymonkey.com"});
+	repository.save(function (err) {
+		if (err)
+			res.send("Shit");
+		res.send(repository);
+	});
+});
+
+module.exports = router;
