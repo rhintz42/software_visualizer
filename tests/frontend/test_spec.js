dd53db93c535bfb0c21a17fb86afa1ddd4c500bd
Robert Hintz <roberth@surveymonkey.com>
2014-05-18 01:51:23 -0700

    Add first steps for testing

---
+++ tests/frontend/test_spec.js
@@ -0,0 +1,13 @@
+describe("Gallery", function() {
+    it("should be true", function() {
+        var a = 10;
+        
+        expect(a).toEqual(10);
+    });
+
+    it("should be true again", function() {
+        var a = 10;
+        
+        expect(a).toEqual(11);
+    });
+});
